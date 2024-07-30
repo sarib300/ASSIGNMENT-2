09cf31e4c9a87ef2a302feca6a2fc5aaf07d4095
SARIB <muhammadsarib942@gmai.com>
2024-07-30 22:58:29 +0500

    FINISHED

---
+++ app/src/main/java/com/example/assignment_2/DetailsActivity.java
@@ -0,0 +1,15 @@
+package com.example.assignment_2;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.assignment_2.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
---
+++ app/src/main/java/com/example/assignment_2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.assignment_2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnGoToDetails = findViewById(R.id.btnGoToDetails);
+        btnGoToDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
